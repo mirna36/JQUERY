593b4ad6f4c7331cf54cb50a9b0115072ef5e6f7
mirna36 <69789618+mirna36@users.noreply.github.com>
2020-10-22 18:30:15 +0200

    v1

---
+++ cours-jquery/02-selecteurs/script.js
@@ -0,0 +1,21 @@
+// LES SELECTEURS EN jQuery
+$(document).ready(function (){
+    //Cibler un élément html
+   // $("p").hide();
+
+    //Cibler un élément html a partir de son id
+    // $("#test").hide();
+
+    //Cibler un élément html a partir de sa class
+     //$(".maClasse").hide();
+
+    //Cibler toute la page html
+    //     //$("*").hide();
+
+    //Cibler le premier élément html d'une class
+    //$("p:first").hide();
+
+    //Cibler le premier élément html d'une class
+    $("ul li:first").hide();
+});
+
